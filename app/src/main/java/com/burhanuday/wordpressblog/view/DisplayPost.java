5952731542e1cd869baec379ba15110307a34a00
Burhanuday <udaipurwalaburhanuddin@gmail.com>
2018-11-20 19:53:43 +0530

    removed agentweb and using WebViewSuite instead

--- app/src/main/java/com/burhanuday/wordpressblog/view/DisplayPost.java
+++ app/src/main/java/com/burhanuday/wordpressblog/view/DisplayPost.java
@@ -1,12 +1,16 @@
 package com.burhanuday.wordpressblog.view;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
 
+import com.asksira.webviewsuite.WebViewSuite;
 import com.burhanuday.wordpressblog.R;
 import com.burhanuday.wordpressblog.network.ApiClient;
 import com.burhanuday.wordpressblog.network.ApiService;
@@ -21,57 +25,56 @@ import io.reactivex.schedulers.Schedulers;
 
 public class DisplayPost extends AppCompatActivity {
 
-    private ApiService apiService;
-    private CompositeDisposable compositeDisposable = new CompositeDisposable();
-    final static String mimeType = "text/html";
-    final static String encoding = "UTF-8";
-
-    @BindView(R.id.wv_body)
-    WebView body;
+    @BindView(R.id.webViewSuite)
+    WebViewSuite webViewSuite;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_display_post);
         ButterKnife.bind(this);
-
-        apiService = ApiClient.getClient(getApplicationContext()).create(ApiService.class);
 
         int id = getIntent().getIntExtra("_id", 0);
         String link = getIntent().getStringExtra("_link");
 
-        if (link!=null && !link.isEmpty()){
-            body.setWebViewClient(new WebViewClient());
-            WebSettings settings = body.getSettings();
-            settings.setJavaScriptCanOpenWindowsAutomatically(true);
-            settings.setSupportMultipleWindows(true);
-            settings.setBuiltInZoomControls(false);
-            settings.setJavaScriptEnabled(true);
-            settings.setAppCacheEnabled(true);
-            settings.setAppCacheMaxSize(10 * 1024 * 1024);
-            settings.setAppCachePath("");
-            settings.setDatabaseEnabled(true);
-            settings.setDomStorageEnabled(true);
-            settings.setGeolocationEnabled(true);
-            settings.setSaveFormData(false);
-            settings.setSavePassword(false);
-            settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
-            // Flash settings
-            settings.setPluginState(WebSettings.PluginState.ON);
-            body.loadUrl(link);
+        webViewSuite.startLoading(link);
+        webViewSuite.interfereWebViewSetup(new WebViewSuite.WebViewSetupInterference() {
+            @Override
+            public void interfereWebViewSetup(WebView webView) {
+                WebSettings settings = webView.getSettings();
+                //Change your WebView settings here
+                settings.setJavaScriptCanOpenWindowsAutomatically(true);
+                settings.setSupportMultipleWindows(true);
+                settings.setBuiltInZoomControls(false);
+                settings.setJavaScriptEnabled(true);
+                settings.setAppCacheEnabled(true);
+                settings.setAppCacheMaxSize(10 * 1024 * 1024);
+                settings.setAppCachePath("");
+                settings.setDatabaseEnabled(true);
+                settings.setDomStorageEnabled(true);
+                settings.setGeolocationEnabled(true);
+                settings.setSaveFormData(false);
+                settings.setSavePassword(false);
+                settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+                // Flash settings
+                settings.setPluginState(WebSettings.PluginState.ON);
+            }
+        });
+        webViewSuite.setOpenPDFCallback(new WebViewSuite.WebViewOpenPDFCallback() {
+            @Override
+            public void onOpenPDF() {
+                finish();
+            }
+        });
 
-            // Geo location settings
-            //settings.setGeolocationEnabled(true);
-            //settings.setGeolocationDatabasePath("/data/data/selendroid");
-        }
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        if (body.canGoBack()){
-            body.goBack();
-        }
+        if (!webViewSuite.goBackIfPossible()) super.onBackPressed();
     }
 }
